3d1c73599e8c743d6eabf583c6aa4f1d4e3518ac
AZhurav1ev <37408110+AZhurav1ev@users.noreply.github.com>
2019-12-13 11:36:28 +0300

    Creating SignIn and CustomButton components

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component'
 import { Route, Switch } from 'react-router-dom';
 import ShopPage from './pages/shoppage/shop.component';
-import Header from './components/header/header.component'
+import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 
 function App() {
@@ -12,7 +13,8 @@ function App() {
         <Header />
       <Switch>
         <Route exact path='/' component={HomePage}/>
-        <Route exact path='/shop' component={ShopPage}/>
+        <Route path='/shop' component={ShopPage}/>
+        <Route path='/signin' component={SignInAndSignUpPage}/>
       </Switch>
     </div>
   );
