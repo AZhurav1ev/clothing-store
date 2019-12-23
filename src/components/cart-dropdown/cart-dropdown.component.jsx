2bb3e61ef180834a3506e3c84508505ac0a13563
AZhurav1ev <37408110+AZhurav1ev@users.noreply.github.com>
2019-12-23 09:36:28 +0300

    update cart-dropdown and cart-icon components with reselect library

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import './cart-dropdown.styles.scss';
 import CustomButton from '../custom-button/custom-button.component';
+import CartItem from '../cart-item/cart-item.component';
+import { connect } from 'react-redux';
+import { selectCartItems } from '../../redux/cart/cart.selectors';
 
-const CartDropdown = () => (
+const CartDropdown = ({ cartItems }) => (
     <div className="cart-dropdown">
         <div className="cart-items">
-            <CustomButton>CHECKOUT</CustomButton>
+            {
+                cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem} />)
+            }
         </div>
+        <CustomButton>CHECKOUT</CustomButton>
     </div>
 )
 
-export default CartDropdown;
+const mapStateToProps = (state) => ({
+    cartItems: selectCartItems(state)
+})
+
+export default connect(mapStateToProps)(CartDropdown);
